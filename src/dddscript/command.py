e6c8223d37b4f6ad35f8f9bc8db8fde42ca5e78d
Samuel Lampa <samuel.lampa@gmail.com>
2012-08-17 14:55:48 +0200

    Sanitize content of list of strings

--- src/dddscript/command.py
+++ src/dddscript/command.py
@@ -4,7 +4,7 @@ Created on Aug 17, 2012
 @author: samuel
 '''
 
-import subprocess
+import subprocess, re
 
 class Command(object):
     '''
@@ -12,15 +12,23 @@ class Command(object):
     (system calls) in different ways.
     '''
 
-    def __init__(self, command_text):
+    def __init__(self, command_txt):
         '''
         Constructor
         '''
-        self.command = command_text
+        # command_txt = self.escape_unallowed_chars(command_txt)
+        command_lst = command_txt.split(" ")
+        command_lst = self.escape_unallowed_chars(command_lst)
+        self.command = command_lst
         
     def run_with_output(self):
         proc = subprocess.Popen(self.command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         (stdout, stderr) = proc.communicate()
-        stdout = stdout.rstrip("\n")
-        stderr = stderr.rstrip("\n")
         return (stdout, stderr)
+
+    def escape_unallowed_chars(self, command_lst):
+        for i, part in enumerate(command_lst):
+            part = part.strip()
+            command_lst[i] = re.escape(part)
+        return command_lst
+
